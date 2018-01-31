4fc5a9bc6cdc0eb89816319a71907c248c982b80
v2trang <vylanatrang@yahoo.com>
2018-01-30 17:02:15 -0800

    Initial Commit

---
+++ Flix_App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Flix_App
+//
+//  Created by Vylana Trang on 1/30/18.
+//  Copyright © 2018 CodePath. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
