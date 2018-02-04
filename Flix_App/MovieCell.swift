cb2c32e521802522ae4201bae95a78e0e8648db9
v2trang <vylanatrang@yahoo.com>
2018-02-03 16:52:18 -0800

    Finished Flix App

---
+++ Flix_App/MovieCell.swift
@@ -0,0 +1,28 @@
+//
+//  MovieCell.swift
+//  Flix_App
+//
+//  Created by Vylana Trang on 2/3/18.
+//  Copyright © 2018 CodePath. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var overviewLabel: UILabel!
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Flix_App' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Flix_App
+  pod 'AlamofireImage', '~> 3.1'
+  target 'Flix_AppTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'Flix_AppUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
