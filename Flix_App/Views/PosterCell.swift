56ea2862f22c10426e3ed28873c6d05bf9c14311
v2trang <vylanatrang@yahoo.com>
2018-02-12 19:05:19 -0800

    PA2 complete

---
+++ Flix_App/Views/PosterCell.swift
@@ -0,0 +1,14 @@
+//
+//  PosterCell.swift
+//  Flix_App
+//
+//  Created by Vylana Trang on 2/12/18.
+//  Copyright © 2018 CodePath. All rights reserved.
+//
+
+import UIKit
+
+class PosterCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterImageView: UIImageView!
+}
